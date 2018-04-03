ef4ab57ede47eb7402cefb912a12b45417693f04
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-04-03 17:43:53 -0400

    somestyle

--- assets/main.js
+++ assets/main.js
@@ -38,11 +38,7 @@ for (let i = 0; i < 10; i++) {
     $('#gifView').prepend(main)
 
     $(".gif").on("click", function () {
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
         var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
